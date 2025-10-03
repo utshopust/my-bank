979fd5fb8b5c2a4123f1c36ff66981bc770db355
utshopust <mcutsho96@gmail.com>
2025-10-03 16:32:55 +0600

    Handle Login and redirect to the home page

--- script/login.js
+++ script/login.js
@@ -1,6 +1,20 @@
+document.getElementById("login-btn")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
 
-document.getElementById('login-btn')
-.addEventListener('click',function(){
-console.log('login button clicked');
+        const accountNumber = document.getElementById('account-number').value;
+        const pin = document.getElementById('pin').value;
 
-})
+        if(accountNumber.length===11){
+            console.log('you are ready to go');
+            if(parseInt(pin)===1234){
+            window.location.href="./main.html"    
+            }
+            else{
+                alert('Invalid Pin');
+            }
+        }
+        else{
+            alert('invalid Account Number');
+        }
+});
