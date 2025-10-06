444ad52f4b2d1f618740fac16c221d78af7863d1
utshopust <mcutsho96@gmail.com>
2025-10-06 11:52:49 +0600

    Dynamic DOM and data validation

--- script/addMoney.js
+++ script/addMoney.js
@@ -10,17 +10,28 @@ document
     const account = document.getElementById("account-number").value;
     const mainBalance = document.getElementById("main-balance").innerText;
     convertedMainBalance = parseInt(mainBalance);
+    const selectedBank = document.getElementById("allBank").value;
+    
+
+    if(amount<0){
+      alert("Invalid Amount");
+      return;
+    }
 
     if (account.length === 11) {
       if (convertedPin === 1234) {
         const sum = convertedMainBalance + convertedAmount;
         document.getElementById("main-balance").innerText = sum;
         const container = document.getElementById("transaction-container");
-        const p = document.createElement("p");
-        p.innerText = `
-        Added ${amount} from ${account} account.
+        const div = document.createElement("div");
+        div.classList.add("border", "p-2", "mb-2","rounded", "bg-green-100");
+        div.innerHTML = `
+        <p class="font-bold text-lg">Transaction Successful</p>
+        <p>Amount: ${amount}</p>
+        <p>From: ${selectedBank} - ${account} </p>
         `;
-        container.append(p);
+        container.append(div);
+        // document.getElementById("add-money-form").reset();
         
       } else {
         alert("wrong pin");
--- script/cashout.js
+++ script/cashout.js
@@ -2,27 +2,36 @@ document
   .getElementById("cash-out-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    
+
     const amount = document.getElementById("cash-out-amount").value;
     const convertedAmount = parseFloat(amount);
     const pin = document.getElementById("pin").value;
     const convertedPin = parseInt(pin);
-        const account = document.getElementById("account-number").value;
+    const account = document.getElementById("account-number").value;
 
     const mainBalance = document.getElementById("main-balance").innerText;
     convertedMainBalance = parseInt(mainBalance);
+    const selectedBank = document.getElementById("allBank").value;
+
+    if (amount > convertedMainBalance) {
+      alert("Insufficient Balance");
+      return;
+    }
 
     if (account.length === 11) {
       if (convertedPin === 1234) {
         const sum = convertedMainBalance - convertedAmount;
         document.getElementById("main-balance").innerText = sum;
         const container = document.getElementById("transaction-container");
-        const p = document.createElement("p");
-        p.innerText = `
-        CashOut ${amount} from ${account} account.
+        const div = document.createElement("div");
+        div.classList.add("border", "p-2", "mb-2", "rounded", "bg-red-100");
+        div.innerHTML = `
+        <p class="font-bold text-lg">Cash-Out Successful</p>
+        <p>Amount: ${amount}</p>
+        <p>From: ${selectedBank} - ${account} </p>
         `;
-        container.append(p);
-        
+        container.append(div);
+        // document.getElementById("add-money-form").reset();
       } else {
         alert("wrong pin");
       }
